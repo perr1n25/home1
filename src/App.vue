f604a26b9f6881df5320e51293ea47ac8b357481
perr1n25 <perr1n@mail.ru>
2020-07-05 21:48:11 +0700

    1.0.1

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 @click="clickCaption" :class="classCaption">{{ textCaption }}</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data: () => {
+    return {
+      textCaption: 'Заголовок',
+      classCaption: 'colorGreen'
+    }
+  },
+  methods: {
+    clickCaption: () => {
+      console.log('Проверка связи')
+    }
+  },
   components: {
-    HelloWorld
+
   }
+
 }
 </script>
 
@@ -24,5 +35,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1.colorGreen {
+  color: green;
 }
 </style>
